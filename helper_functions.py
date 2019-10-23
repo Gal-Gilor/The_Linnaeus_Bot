c8332ceef3bdd34e3eea0cbc399ef981f9c81657
Gal Gilor <gilorgal2010@gmail.com>
2019-10-23 16:00:17 -0400

    small changes in functions, cleaning notebooks and renaming images

--- helper_functions.py
+++ helper_functions.py
@@ -7,12 +7,11 @@ import os
 import plotly.graph_objects as go
 import joblib as jb
 import itertools
-import cv2
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, recall_score
 import pydot
 import tensorflow as tf
-from tensorflow import keras
+from tf import keras
 from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Input, MaxPool2D, UpSampling2D
 from keras.models import Sequential, load_model, Model
 from keras.utils import to_categorical, plot_model, vis_utils
@@ -22,7 +21,7 @@ from keras import backend as k
 plt.rcParams['axes.spines.right'] = False
 plt.rcParams['axes.spines.top'] = False
 
-## Image processing
+# Image processing
 
 
 def process_images(inpath, outpath, dim_tuple, start=1, extension='jpg'):
@@ -38,22 +37,21 @@ def process_images(inpath, outpath, dim_tuple, start=1, extension='jpg'):
     Returns:
         Returns a folder containing double amount of images
     '''
-    #open images
+    # open images
     for file in tqdm(glob(f'{inpath}*.{extension}')):
         with Image.open(file) as img:
-            #rotate image
+            # rotate image
             rotated_images = rotate_images(img)
-            
+
             # resize images
             resized = resizing(rotated_images, dim_tuple)
-            
-            #grayscale images
+
+            # grayscale images
             gray_images = grayscale(resized)
-            
-            
+
             # save the images
             save_preprocessed_images(gray_images, outpath, extension, start)
-            start += 2                      
+            start += 2
     return
 
 
@@ -67,10 +65,10 @@ def rotate_images(image):
         A list of images containing the original image and the rotated one
     '''
     rotated_images = []
-    
+
     chirl_image = image.transpose(Image.FLIP_LEFT_RIGHT)
-    
-    rotated_images.extend([image, chirl_image])  
+
+    rotated_images.extend([image, chirl_image])
     return rotated_images
 
 
@@ -112,7 +110,8 @@ def save_preprocessed_images(processed_images, outpath, start, extension='jpg'):
     Returns:
         Does not return a variable. Creates image files 
     '''
-    [image.save(f'{outpath}\\image{i}.{extension}') for i, image in enumerate(processed_images, start)]
+    [image.save(f'{outpath}\\image{i}.{extension}')
+     for i, image in enumerate(processed_images, start)]
     return
 
 
@@ -124,7 +123,7 @@ def save_train_test(inpath, outpath, n, j, extension='jpg'):
         outpath: The path where you want to save the file
         n: int. slicing the matrix
         j: int. slicing the matrix
-            
+
     '''
     images = []
     for file in tqdm(glob(f'{inpath}*.{extension}')[n:j]):
@@ -132,14 +131,14 @@ def save_train_test(inpath, outpath, n, j, extension='jpg'):
             np_image = np.array(img) / 255
             np_image = np_image.expand_dims(np_image, axis=0)
             images.append(np_image)
-    
-    images = np.asarray(images)   
+
+    images = np.asarray(images)
     np.save(outpath, images)
     del images
     return
 
 
-## Image classification
+# Image classification
 
 
 def load_data(damselpath, dragonpath, start, end):
@@ -155,9 +154,10 @@ def load_data(damselpath, dragonpath, start, end):
         Retuns a concatenated numpy array with dragonfly and damselfly images (dragons first),
         and a vector the size of the image vector where dragons value is 1 and damsel 0
     '''
-    data = np.concatenate((np.load(dragonpath)[start:end], np.load(damselpath)[start:end]), axis=0)
+    data = np.concatenate(
+        (np.load(dragonpath)[start:end], np.load(damselpath)[start:end]), axis=0)
     dragon_images = len(np.load(dragonpath)[start:end])
-    labels = labels_vector(data, dragon_images)                           
+    labels = labels_vector(data, dragon_images)
     return data, labels
 
 
@@ -194,27 +194,28 @@ def create_model(weights_path=None, weights_name=None):
     model.add(Conv2D(32, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.4))
-    
+
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.4))
-    
+
     model.add(Conv2D(128, (3, 3), activation='relu'))
     model.add(Conv2D(128, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.4))
-    
+
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.4))
-    
+
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.4))
 
     model.add(Dense(2, activation='sigmoid'))
-    
-    model.compile(loss='binary_crossentropy', optimizer='Adam', metrics=['accuracy'])
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='Adam', metrics=['accuracy'])
     # optional: loading previous weights
     if weights_path and weights_name:
         model.load_weights(f'{weights_path}{weights_name}')
@@ -232,18 +233,19 @@ def create_train_validation(training_data, labels):
         Shuffled, reshaped arrays with matching labels
     '''
     # split to sets
-    x_train, x_val, y_train, y_val = train_test_split(training_data, labels, test_size=0.05)
-    
-    # adjust variables dimensions to fit into the cnn    
+    x_train, x_val, y_train, y_val = train_test_split(
+        training_data, labels, test_size=0.05)
+
+    # adjust variables dimensions to fit into the cnn
     x_train = np.expand_dims(x_train, axis=-1)
     x_val = np.expand_dims(x_val, axis=-1)
-    
+
     y_train_cat = to_categorical(y_train)
     y_val_cat = to_categorical(y_val)
     return x_train, x_val, y_train, y_val, y_train_cat, y_val_cat
 
 
-## Image visualisation
+# Image visualisation
 
 
 def load_model_stats(path):
@@ -258,25 +260,24 @@ def load_model_stats(path):
     '''
     # load the files
     history = [jb.load(file) for file in glob(path)]
-    
+
     # combine the accuracy and loss changes throughout the entire model training
     # training accuracy
     train_acc = [keras_object.history['acc'] for keras_object in history]
     train_acc = list(itertools.chain.from_iterable(train_acc))
-    
+
     # validation accuracy
     val_acc = [keras_object.history['val_acc'] for keras_object in history]
     val_acc = list(itertools.chain.from_iterable(val_acc))
-    
+
     # training loss
     train_loss = [keras_object.history['loss'] for keras_object in history]
     train_loss = list(itertools.chain.from_iterable(train_loss))
-    
+
     # validation loss
     val_loss = [keras_object.history['val_loss'] for keras_object in history]
     val_loss = list(itertools.chain.from_iterable(val_loss))
     return [train_acc, val_acc], [train_loss, val_loss]
-
 
 
 def plot_train_history(data, title, ytitle, xtitle, save=False):
@@ -293,27 +294,26 @@ def plot_train_history(data, title, ytitle, xtitle, save=False):
         Time series plot showing the change in accuracy or loss throughout the model training
     '''
     fig = go.Figure()
-    
+
     # add traces
     fig.add_trace(go.Scatter(y=data[0],
-                  mode='lines',
-                  name=f'Training {ytitle}'))
-    
+                             mode='lines',
+                             name=f'Training {ytitle}'))
+
     fig.add_trace(go.Scatter(y=data[1],
-                  mode='lines',
-                  name=f'Validation {ytitle}'))
-        
+                             mode='lines',
+                             name=f'Validation {ytitle}'))
 
     # add labels, change figure/font sizes, and remove grid lines
     fig.update_layout(autosize=False,
                       width=600,
                       height=550,
-                       
+
                       xaxis=go.layout.XAxis(
                           title=xtitle,
                           titlefont=dict(size=18),
                           showgrid=False),
-                      
+
                       yaxis=go.layout.YAxis(
                           title=ytitle,
                           titlefont=dict(size=18),
@@ -348,17 +348,17 @@ def plot_bar_graph(x, y, title, ytitle, xtitle, save=False):
         Bar graph showing the amount of images in each class
     '''
     fig = go.Figure()
-    
+
     # add trace
-    fig.add_trace(go.Bar(x = x,
-                         y = y)
-                 )
-    
+    fig.add_trace(go.Bar(x=x,
+                         y=y)
+                  )
+
     # add labels, change figure/font sizes, and remove grid lines
     fig.update_layout(autosize=False,
                       width=600,
                       height=550,
-                      
+
                       xaxis=go.layout.XAxis(
                           title_=xtitle,
                           titlefont=dict(size=18),
@@ -427,15 +427,14 @@ def create_unsupervised_model(weights_path=None, weights_name=None, shape=(256, 
 
     # define model input and output
     model = Model(input_dim, decoded)
-    
-    #compile
+
+    # compile
     model.compile(optimizer='Adam', loss='mse')
     # optional: loading previous weights
     if weights_path and weights_name:
         model.load_weights(f'{weights_path}{weights_name}')
-        
-    return model
 
+    return model
 
 
 def plot_intermediate_activation(model, images, bottom=0, top=3, save=False):
@@ -452,37 +451,37 @@ def plot_intermediate_activation(model, images, bottom=0, top=3, save=False):
          Images displaying what each convolution layer detects        
     '''
     images = np.expand_dims(images, axis=-1)
-    
+
     # extract layer's outputs
     layer_outputs = [layer.output for layer in model.layers[bottom:top]]
-    
-    # Creates keras Model 
+
+    # Creates keras Model
     activation_model = Model(inputs=model.input, outputs=layer_outputs)
-    activations = activation_model.predict(images) 
+    activations = activation_model.predict(images)
     layer_names = [layer.name for layer in model.layers[bottom:top]]
-    
+
     # Define how the amount of images per row
     images_per_row = 16
-    
-    for layer_name, layer_activation in zip(layer_names, activations): 
-        n_features = layer_activation.shape[-1] # number of features
-        size = layer_activation.shape[1] # feature map 
-        n_cols = n_features // images_per_row # tiles the activation channels 
+
+    for layer_name, layer_activation in zip(layer_names, activations):
+        n_features = layer_activation.shape[-1]  # number of features
+        size = layer_activation.shape[1]  # feature map
+        n_cols = n_features // images_per_row  # tiles the activation channels
         display_grid = np.zeros((size * n_cols, images_per_row * size))
-        
-        for col in range(n_cols): # Tiles each filter into a big horizontal grid
+
+        for col in range(n_cols):  # Tiles each filter into a big horizontal grid
             for row in range(images_per_row):
                 # post-processes the features to visualize the activation layers
                 channel_image = layer_activation[0, :, :,
                                                  col * images_per_row + row]
-                channel_image -= channel_image.mean() 
-                channel_image /= channel_image.std()  
-                channel_image *= 64                   
-                channel_image += 128                
+                channel_image -= channel_image.mean()
+                channel_image /= channel_image.std()
+                channel_image *= 64
+                channel_image += 128
                 channel_image = np.clip(channel_image, 0, 255).astype('uint8')
-                display_grid[col * size : (col + 1) * size, 
-                             row * size : (row + 1) * size] = channel_image
-    
+                display_grid[col * size: (col + 1) * size,
+                             row * size: (row + 1) * size] = channel_image
+
         scale = 1. / size
         plt.figure(figsize=(scale * display_grid.shape[1],
                             scale * display_grid.shape[0]))
@@ -509,7 +508,7 @@ def print_metrics(labels, predictions, print_score=True):
 
     recall = round(recall_score(labels, predictions)*100, 2)
     acc = round(accuracy_score(labels, predictions)*100, 2)
-   
+
     if print_score:
         print(f"Recall: {recall}")
         print(f"Accuracy: {acc}")
@@ -525,13 +524,13 @@ def plot_confusion_matrix(y_test, y_pred, class_names, save=None):
         y_pred: list. The model's labels
         class_names: list, the classes names'
         save: Default=None. Saves the image as png
-        
+
     Returns:
         Returns confusion matrix plot
     '''
-    matfig = plt.figure(figsize=(11,10))
-    matrix = hlf.confusion_matrix(y_test, y_pred)
-    plt.matshow(matrix, cmap=plt.cm.Purples, aspect=1.2, 
+    matfig = plt.figure(figsize=(11, 10))
+    matrix = confusion_matrix(y_test, y_pred)
+    plt.matshow(matrix, cmap=plt.cm.Purples, aspect=1.2,
                 alpha=0.6, fignum=matfig.number)
     plt.grid(b=None)
     # add color bar
@@ -541,7 +540,7 @@ def plot_confusion_matrix(y_test, y_pred, class_names, save=None):
     plt.title('Confusion Matrix', fontsize=20)
     plt.ylabel('Actual', fontsize=16)
     plt.xlabel('Predicted', fontsize=16)
-
+    plt.grid(b=None)
     # add appropriate Axis Scales
     tick_marks = np.arange(len(class_names))
     plt.xticks(tick_marks, class_names, rotation=45)
@@ -549,12 +548,12 @@ def plot_confusion_matrix(y_test, y_pred, class_names, save=None):
     plt.grid(b=None)
 
     # add Labels to Each Cell
-    thresh = matrix.max() / 2.  
+    thresh = matrix.max() / 2.
 
     # iterate through the confusion matrix and append the labels
-    for i, j in itertools.product(range(matrix.shape[0]), 
+    for i, j in itertools.product(range(matrix.shape[0]),
                                   range(matrix.shape[1])):
-        plt.text(j, i, matrix[i, j], 
+        plt.text(j, i, matrix[i, j],
                  horizontalalignment="center",
                  color="black")
 
@@ -563,6 +562,8 @@ def plot_confusion_matrix(y_test, y_pred, class_names, save=None):
         plt.grid(b=None)
         plt.tight_layout()
         plt.savefig(f'{save}_cm.png')
+    plt.grid(b=None)
+    plt.show()
     return
 
 
@@ -577,7 +578,7 @@ def find_wrong_classification(images, labels, predictions):
     Returns:
         All the misclassified images
     '''
-    indices =  np.where(labels != predictions)
+    indices = np.where(labels != predictions)
     return images[indices]
 
 
@@ -593,17 +594,17 @@ def plot_bar_graph(x, y, title, ytitle, xtitle):
         xtitle: X axis title (string)
     '''
     fig = go.Figure()
-    
+
     # add trace
-    fig.add_trace(go.Bar(x = x,
-                         y = y)
-                 )
-    
+    fig.add_trace(go.Bar(x=x,
+                         y=y)
+                  )
+
     # add labels, change figure/font sizes, and remove grid lines
     fig.update_layout(autosize=False,
                       width=600,
                       height=550,
-                      
+
                       xaxis=go.XAxis(
                           title=xtitle,
                           titlefont=dict(size=18),
@@ -640,13 +641,14 @@ def save_predict_class(model, images, labels, path):
         misclassified images
     '''
     dictionary = {}
-    
+
     # predict
+    labels_cat = to_categorical(labels)
     preds = model.predict(images)
     classes = model.predict_classes(images)
-    acc, loss = model.evaluate(images, labels)
+    loss, acc = model.evaluate(images, labels_cat)
     misclassified = find_wrong_classification(images, labels, classes)
-    
+
     # compile the results
     dictionary['raw'] = images
     dictionary['n_images'] = len(labels)
@@ -656,7 +658,7 @@ def save_predict_class(model, images, labels, path):
     dictionary['pred_class'] = classes
     dictionary['actual'] = labels
     dictionary['misclassified'] = misclassified
-    
+
     # save the condensed results
     jb.dump(dictionary, path)
     print(f'Prediction Accuracy: {acc * 100}%')
